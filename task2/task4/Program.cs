7785418edf0c159dbe46f1e467552c17e72cd4eb
Julia <log6117@yandex.ru>
2023-06-14 02:30:22 +0300

    task4

---
+++ task2/task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает 
+// число (N), а на выходе показывает все чётные числа 
+// от 1 до N.
+Console.WriteLine ("input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+while (current <= number - 2)
+{
+    current = current + 1;
+    if (current%2 == 0)
+{
+    Console.Write(current + ", ");
+}
+}
